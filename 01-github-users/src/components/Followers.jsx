c78224db6f79d432847802040105f48fe19cf195
Margus Liinev <margus.liinev@gmail.com>
2023-02-18 23:33:42 +0200

    styles fix

--- 01-github-users/src/components/Followers.jsx
+++ 01-github-users/src/components/Followers.jsx
@@ -7,18 +7,20 @@ const Followers = () => {
 
     return (
         <article className='followers'>
-            {followers.map((follower, index) => {
-                const { avatar_url: img, html_url: url, login: login } = follower;
-                return (
-                    <div key={index}>
-                        <img src={img} alt={login} />
-                        <div>
-                            <h6>{login}</h6>
-                            <a href={url}>{url}</a>
+            <div className='followers-container'>
+                {followers.map((follower, index) => {
+                    const { avatar_url: img, html_url: url, login: login } = follower;
+                    return (
+                        <div key={index}>
+                            <img src={img} alt={login} />
+                            <div>
+                                <h6>{login}</h6>
+                                <a href={url}>{url}</a>
+                            </div>
                         </div>
-                    </div>
-                );
-            })}
+                    );
+                })}
+            </div>
         </article>
     );
 };
