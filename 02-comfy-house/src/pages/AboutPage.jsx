aee67d15605a3c3c6ba9b0a3b4d665fde34956ff
Margus Liinev <margus.liinev@gmail.com>
2023-02-25 01:46:22 +0200

    about page

--- 02-comfy-house/src/pages/AboutPage.jsx
+++ 02-comfy-house/src/pages/AboutPage.jsx
@@ -10,6 +10,7 @@ const AboutPage = () => {
                 <img src={aboutImg} alt='furniture image' className='about-img' />
                 <div className='about-info'>
                     <h3>Our Story</h3>
+                    <div className='title-underline'></div>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis tempore eos cumque cupiditate quia facere labore. Fuga explicabo delectus expedita, ducimus, pariatur modi nisi rem veritatis, assumenda molestias libero facere obcaecati consequuntur temporibus eum soluta velit molestiae quam ipsa? Dolorem culpa at laboriosam aspernatur debitis! Suscipit voluptatum asperiores ab quasi!</p>
                 </div>
             </div>
