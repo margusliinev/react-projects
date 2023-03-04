fff23067b7cc67d003832938e46eebf6ec84e0e2
Margus Liinev <margus.liinev@gmail.com>
2023-03-04 16:50:16 +0200

    finished productimages component

--- 02-comfy-house/src/components/ProductImages.jsx
+++ 02-comfy-house/src/components/ProductImages.jsx
@@ -1,11 +1,15 @@
+import React, { useState } from 'react';
+
 const ProductImages = ({ images }) => {
+    const [mainImage, setMainImage] = useState(0);
+
     return (
         <div className='single-product-img-container'>
-            <img src={images ? images[0].url : ''} alt={name} className='product-main-img' />
+            <img src={images ? images[mainImage].url : ''} alt='main image' className='product-main-img' />
             <div className='images-container'>
                 {images
-                    ? images.map((image) => {
-                          return <img key={image.id} src={image.url} alt='product image' className='product-alt-img' />;
+                    ? images.map((image, index) => {
+                          return <img key={index} src={image.url} alt='product image' className={mainImage === index ? 'product-alt-img active' : 'product-alt-img'} onClick={() => setMainImage(index)} />;
                       })
                     : []}
             </div>
