186e9501390f1d64a10c2fd2884af74f7361e29e
Margus Liinev <margus.liinev@gmail.com>
2023-03-07 16:48:25 +0200

    sorting component styles

--- 02-comfy-house/src/components/Sort.jsx
+++ 02-comfy-house/src/components/Sort.jsx
@@ -1,5 +1,32 @@
+import React, { useContext } from 'react';
+import { FilterContext } from '../context/FilterContext';
+import { BsFillGridFill, BsList } from 'react-icons/bs';
+
 const Sort = () => {
-    return <div>Sort</div>;
+    const { filtered_products, all_products, grid_view } = useContext(FilterContext);
+    return (
+        <div className='sort'>
+            <div className='sort-buttons-container'>
+                <button type='button' className={grid_view ? 'grid-button-active' : 'grid-button'}>
+                    <BsFillGridFill />
+                </button>
+                <button type='button' className={!grid_view ? 'list-button-active' : 'list-button'}>
+                    <BsList />
+                </button>
+            </div>
+            <p className='products-count'>{filtered_products.length} Products Found</p>
+            <div className='connector'></div>
+            <div>
+                <label htmlFor='sort'>Sort By </label>
+                <select name='sort' id='sort'>
+                    <option value='price-lowest'>Price (Lowest)</option>
+                    <option value='price-highest'>Price (Highest)</option>
+                    <option value='name-a-z'>Name (A - Z)</option>
+                    <option value='name-z-a'>Name (Z - A)</option>
+                </select>
+            </div>
+        </div>
+    );
 };
 
 export default Sort;
