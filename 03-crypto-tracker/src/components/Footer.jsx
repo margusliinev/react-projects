c8a8c6d75e342642f52372f04895a644d9cad524
Margus Liinev <margus.liinev@gmail.com>
2023-03-17 23:27:29 +0200

    Footer finished

--- 03-crypto-tracker/src/components/Footer.jsx
+++ 03-crypto-tracker/src/components/Footer.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
 const Footer = () => {
-    return <div>Footer</div>;
+    return (
+        <footer className='footer'>
+            <h6>
+                &copy; {new Date().getFullYear()}
+                <span>
+                    {' '}
+                    Crypto<span>Tracker</span>
+                </span>
+            </h6>
+            <h6>All rights reserved</h6>
+        </footer>
+    );
 };
 
 export default Footer;
