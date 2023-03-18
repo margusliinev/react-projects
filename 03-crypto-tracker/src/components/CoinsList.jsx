524a2d2cb0430a609cfd076635f843f500f6feec
Margus Liinev <margus.liinev@gmail.com>
2023-03-19 01:50:17 +0200

    coinsList component finished

--- 03-crypto-tracker/src/components/CoinsList.jsx
+++ 03-crypto-tracker/src/components/CoinsList.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Loader } from '../components';
+import { formatPrice } from '../utils/formatPrice';
+import { formatPriceBillion } from '../utils/formatPriceBillion';
 import { RxTriangleDown, RxTriangleUp } from 'react-icons/rx';
+import { HiPlus } from 'react-icons/hi';
 
 const CoinsList = () => {
     const { coins_loading, coins_error, coins } = useSelector((store) => store.coins);
     console.log(coins);
+
+    if (coins_loading) {
+        return <Loader />;
+    }
+
+    if (coins_error) {
+        return;
+    }
+
     return (
         <div className='coins-list'>
             {coins.map((coin, index) => {
@@ -16,11 +28,18 @@ const CoinsList = () => {
                             <img className='coin-image' src={coin.image} alt={coin.name} />
                             <p>{coin.name}</p>
                         </div>
-                        <div className={coin.market_cap_change_percentage_24h > 0 ? 'coin-growth box-green' : 'coin-growth box-red'}>
+                        <div className={coin.market_cap_change_percentage_24h > 0 ? 'coin-growth box-green value-green' : 'coin-growth box-red value-red'}>
                             {coin.market_cap_change_percentage_24h > 0 ? <RxTriangleUp /> : <RxTriangleDown />}
                             <span>{`${Math.abs(coin.market_cap_change_percentage_24h.toFixed(2))}%`}</span>
                         </div>
-                        <p className='coin-price'>{`€${coin.current_price}`}</p>
+                        <p className='coin-price'>{formatPrice(coin.current_price)}</p>
+                        <p className='coin-price-btc'>{(coin.current_price / coins[0].current_price).toFixed(8)}</p>
+                        <p className='coin-market-cap'>{formatPriceBillion(coin.market_cap)}</p>
+                        <p className='coin-price-low'>{formatPrice(coin.low_24h)}</p>
+                        <p className='coin-price-high'>{formatPrice(coin.high_24h)}</p>
+                        <button type='button' className='add-to-tracker'>
+                            <HiPlus />
+                        </button>
                     </article>
                 );
             })}
---
+++ 03-crypto-tracker/src/utils/formatPriceBillion.jsx
@@ -0,0 +1,13 @@
+const formatPriceBillion = (num) => {
+    if (num >= 1000000000) {
+        const formattedNum = (num / 1000000000).toFixed(1);
+        return `${formattedNum}B`;
+    } else if (num >= 1000000) {
+        const formattedNum = (num / 1000000).toFixed(1);
+        return `${formattedNum}M`;
+    } else {
+        return num;
+    }
+};
+
+export { formatPriceBillion };
