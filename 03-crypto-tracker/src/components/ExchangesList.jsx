55c7ab4cde3e9326bc4b849558944208b3ef112a
Margus Liinev <margus.liinev@gmail.com>
2023-04-08 14:14:48 +0300

    exchanges list responsive design, first design

--- 03-crypto-tracker/src/components/ExchangesList.jsx
+++ 03-crypto-tracker/src/components/ExchangesList.jsx
@@ -16,29 +16,33 @@ const ExchangesList = () => {
                                   <h4 className='exchange-title'>{exchange.name}</h4>
                               </div>
                               <div className='exchange-data-container'>
-                                  <div className='exchange-data'>
-                                      <p className='exchange-data-title'>Trust Score Rank</p>
-                                      <p className='exchange-data-value'>{exchange.trust_score_rank}</p>
+                                  <div className='data-container-column-one'>
+                                      <div className='exchange-data'>
+                                          <p className='exchange-data-title'>Trust Score Rank</p>
+                                          <p className='exchange-data-value'>{!exchange.trust_score_rank ? 250 : exchange.trust_score_rank}</p>
+                                      </div>
+                                      <div className='exchange-data'>
+                                          <p className='exchange-data-title'>Trust Score</p>
+                                          <p className='exchange-data-value'>{!exchange.trust_score ? 10 : exchange.trust_score}</p>
+                                      </div>
+                                      <div className='exchange-data'>
+                                          <p className='exchange-data-title'>Trade Volume 24h</p>
+                                          <p className='exchange-data-value'>{!exchange.trade_volume_24h_btc ? 0 + ' BTC' : exchange.trade_volume_24h_btc.toFixed(2) + ' BTC'}</p>
+                                      </div>
                                   </div>
-                                  <div className='exchange-data'>
-                                      <p className='exchange-data-title'>Trade Volume 24h</p>
-                                      <p className='exchange-data-value'>{exchange.trade_volume_24h_btc.toFixed(2) + ' BTC'}</p>
-                                  </div>
-                                  <div className='exchange-data'>
-                                      <p className='exchange-data-title'>Trust Score</p>
-                                      <p className='exchange-data-value'>{exchange.trust_score}</p>
-                                  </div>
-                                  <div className='exchange-data'>
-                                      <p className='exchange-data-title'>Country</p>
-                                      <p className='exchange-data-value'>{exchange.country}</p>
-                                  </div>
-                                  <div className='exchange-data'>
-                                      <p className='exchange-data-title'>Year Established</p>
-                                      <p className='exchange-data-value'>{exchange.year_established}</p>
-                                  </div>
-                                  <div className='exchange-data'>
-                                      <p className='exchange-data-title'>Website</p>
-                                      <p className='exchange-data-value'>{exchange.url}</p>
+                                  <div className='data-container-column-two'>
+                                      <div className='exchange-data'>
+                                          <p className='exchange-data-title'>Year Established</p>
+                                          <p className='exchange-data-value'>{!exchange.year_established ? new Date().getFullYear() : exchange.year_established}</p>
+                                      </div>
+                                      <div className='exchange-data'>
+                                          <p className='exchange-data-title'>Country</p>
+                                          <p className='exchange-data-value'>{!exchange.country ? 'worldwide' : exchange.country}</p>
+                                      </div>
+                                      <div className='exchange-data'>
+                                          <p className='exchange-data-title'>Website</p>
+                                          <p className='exchange-data-value'>{!exchange.url ? 'No Website' : exchange.url}</p>
+                                      </div>
                                   </div>
                               </div>
                           </article>
