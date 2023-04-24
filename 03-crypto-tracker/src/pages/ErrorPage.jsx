decbba8c89b658f61dd756ababd0c14712b75fe1
Margus Liinev <margus.liinev@gmail.com>
2023-04-24 19:45:15 +0300

    added error page, price change boxes component conditional rendering fix

--- 03-crypto-tracker/src/pages/ErrorPage.jsx
+++ 03-crypto-tracker/src/pages/ErrorPage.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const ErrorPage = () => {
-    return <main>ErrorPage</main>;
+    return (
+        <section className='error-page'>
+            <div className='error-page-container'>
+                <h1>404</h1>
+                <h3>Sorry, this page doesn't exist.</h3>
+                <Link to={'/'} className='btn'>
+                    back home
+                </Link>
+            </div>
+        </section>
+    );
 };
 
 export default ErrorPage;
