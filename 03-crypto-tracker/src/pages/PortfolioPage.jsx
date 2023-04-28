d666c6dbdbae057286fd2bba165aca19fc246965
Margus Liinev <margus.liinev@gmail.com>
2023-04-28 16:38:56 +0300

    portfolio page big screen design and small screen base setup

--- 03-crypto-tracker/src/pages/PortfolioPage.jsx
+++ 03-crypto-tracker/src/pages/PortfolioPage.jsx
@@ -18,7 +18,7 @@ const PortfolioPage = () => {
     return (
         <main className='portfolio'>
             <div className='portfolio-container'>
-                <h6>Your Portfolio</h6>
+                <h5>Your Portfolio</h5>
                 <div className='portfolio-list'>
                     {portfolio.length > 0
                         ? portfolio.map((coin) => {
@@ -35,44 +35,44 @@ const PortfolioPage = () => {
                                       </div>
                                       <div className='portfolio-coin-stats'>
                                           <div className='portfolio-coin-market'>
-                                              <p>Market Price</p>
+                                              <p className='portfolio-coin-market-title'>Market Price</p>
                                               <div className='portfolio-coin-market-stats'>
                                                   <div className='portfolio-coin-stat'>
-                                                      <p>Price now:</p>
-                                                      <p>€26,632</p>
+                                                      <p className='portfolio-coin-stat-title'>Price now:</p>
+                                                      <p className='portfolio-coin-stat-value'>€26,632</p>
                                                   </div>
                                                   <div className='portfolio-coin-stat'>
-                                                      <p>All time High:</p>
-                                                      <p>€59717</p>
+                                                      <p className='portfolio-coin-stat-title'>All time High:</p>
+                                                      <p className='portfolio-coin-stat-value'>€59717</p>
                                                   </div>
                                                   <div className='portfolio-coin-stat'>
-                                                      <p>Price 24h Change:</p>
-                                                      <p>2.33%</p>
+                                                      <p className='portfolio-coin-stat-title'>Price 24h Change:</p>
+                                                      <p className='portfolio-coin-stat-value'>2.33%</p>
                                                   </div>
                                                   <div className='portfolio-coin-stat'>
-                                                      <p>Circ/Total Sup:</p>
-                                                      <p>progress bar</p>
+                                                      <p className='portfolio-coin-stat-title'>Circ/Total Sup:</p>
+                                                      <p className='portfolio-coin-stat-value'>progress bar</p>
                                                   </div>
                                               </div>
                                           </div>
                                           <div className='portfolio-coin-user'>
-                                              <p>Your Coin</p>
+                                              <p className='portfolio-coin-market-title'>Your Coin</p>
                                               <div className='portfolio-coin-user-stats'>
-                                                  <div className='portfolio-coin-user-stat'>
-                                                      <p>Amount</p>
-                                                      <p>1</p>
+                                                  <div className='portfolio-coin-stat'>
+                                                      <p className='portfolio-coin-stat-title'>Amount:</p>
+                                                      <p className='portfolio-coin-stat-value'>1</p>
                                                   </div>
-                                                  <div className='portfolio-coin-user-stat'>
-                                                      <p>Value:</p>
-                                                      <p>€24,222</p>
+                                                  <div className='portfolio-coin-stat'>
+                                                      <p className='portfolio-coin-stat-title'>Value:</p>
+                                                      <p className='portfolio-coin-stat-value'>€24,222</p>
                                                   </div>
-                                                  <div className='portfolio-coin-user-stat'>
-                                                      <p>Price change since purchase</p>
-                                                      <p>-22.33%</p>
+                                                  <div className='portfolio-coin-stat'>
+                                                      <p className='portfolio-coin-stat-title'>Price since purchase:</p>
+                                                      <p className='portfolio-coin-stat-value'>-22.33%</p>
                                                   </div>
-                                                  <div className='portfolio-coin-user-stat'>
-                                                      <p>Purchase Date</p>
-                                                      <p>12-09-2021</p>
+                                                  <div className='portfolio-coin-stat'>
+                                                      <p className='portfolio-coin-stat-title'>Purchase Date:</p>
+                                                      <p className='portfolio-coin-stat-value'>12-09-2021</p>
                                                   </div>
                                               </div>
                                           </div>
